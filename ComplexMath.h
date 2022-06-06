4b35bfacc76454d61812de256a334411f5458c6c
Lolloper <106672777+Lolloper@users.noreply.github.com>
2022-06-06 17:58:01 +0200

    Add files via upload

---
+++ ComplexMath.h
@@ -0,0 +1,58 @@
+#include <math.h>
+#include <stdio.h>
+/* PI value */
+#define CM_PI 3.141592653589793
+//#define CM
+
+
+/* Radians and degrees converter functions */
+#define CM_rad2deg(x) 180*x/B_PI
+#define B_deg2rad(x) B_PI*x/180
+
+/* Complex number struct */
+typedef struct {
+	double Re;
+	double Im;
+} CM_ComplexNumber;
+
+/* Complex number "constructor" */
+CM_ComplexNumber CM_CreateComplexNumber(double r, double i)
+{
+	CM_ComplexNumber n2r;
+	n2r.Re = r;
+	n2r.Im = i;
+	return n2r;
+}
+
+/* Basic automated print function */
+void CM_printcn(CM_ComplexNumber a)
+{
+	printf("Re: %f, Im: %f\n", a.Re, a.Im);
+}
+
+/* Basic arithmetic functions */
+CM_ComplexNumber CM_Sum(CM_ComplexNumber a, CM_ComplexNumber b)
+{
+	return CM_CreateComplexNumber(a.Re+b.Re, a.Im+b.Im);
+}
+CM_ComplexNumber CM_Sub(CM_ComplexNumber a, CM_ComplexNumber b)
+{
+	return CM_CreateComplexNumber(a.Re-b.Re, a.Im-b.Im);
+}
+CM_ComplexNumber CM_Multiply(CM_ComplexNumber a, CM_ComplexNumber b)
+{
+	return CM_CreateComplexNumber((a.Re*b.Re)-(a.Im*b.Im), (a.Re*b.Im)+(a.Im*b.Re));
+}
+CM_ComplexNumber CM_Divide(CM_ComplexNumber a, CM_ComplexNumber b)
+{
+	double d = b.Re*b.Re + b.Im*b.Im;
+	return CM_CreateComplexNumber((a.Re*b.Re+a.Im*b.Im)/d, (a.Im*b.Re-a.Re*b.Im)/d);
+}
+CM_ComplexNumber CM_Pow(CM_ComplexNumber a, CM_ComplexNumber b)
+{
+	double r = sqrt(a.Re*a.Re + a.Im*a.Im);
+	double theta = atan(a.Im/a.Re);
+	double pre = pow(r, b.Re)*exp(-b.Im*theta);
+	double angle = (b.Re*theta) + (b.Im*log(r));
+	return CM_CreateComplexNumber((double) pre*cos(angle), (double) pre*sin(angle));
+}
